7003f1e43b6d4c737ae5f59411624f5fc89b1bd4
akramoo <samirounis2@gmail.com>
2024-03-15 14:34:14 +0100

    adding the styles and the class name from the stylesheet file

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,16 +1,15 @@
-
-
 import Navbar from '../ui/dashboard/navbar/navbar.jsx'
 import Sidebar from '../ui/dashboard/sidebar/sidebar.jsx'
+import styles from '../ui/dashboard/dashboard.module.css'
 
 
 const Layout = ({ children }) => {
     return ( 
-     <div>
-      <div>
+     <div className={styles.container}>
+      <div className={styles.menu}>
         <Navbar />
       </div>
-      <div>
+      <div className={styles.content}>
         <Sidebar />
         {children}
       </div>
