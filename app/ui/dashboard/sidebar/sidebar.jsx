4d83f8f634395c15c11d266df827b9a3010ecc04
akramoo <samirounis2@gmail.com>
2024-03-15 21:05:01 +0100

    styling and adding sidebar content

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,7 +1,35 @@
+// importing the style
+import styles from "./sidebar.module.css";
+
+// importing the object
+import menuItems from "./menuItems.js";
+
+// importing the components
+import MenuLink from "./menuLink/menuLink";
+import Image from "next/image";
+
 const Sidebar = () => {
-    return (
-      <div>Sidebar</div>
-    )
-  }
-  
-  export default Sidebar
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image className={styles.userImage} src="/noavatar.png" height={50} width={50} alt="" />
+        <div className={styles.userDetails}>
+          <span className={styles.userName}>John Joe</span>
+          <span className={styles.userTitle}>Adminstrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
