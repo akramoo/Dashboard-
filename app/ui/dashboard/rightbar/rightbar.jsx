38c791b91e1234fb7c4e1b149c03d4943b7d400a
akramoo <samirounis2@gmail.com>
2024-03-17 16:11:15 +0100

    creating rightbar component

---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,12 @@
+import styles from './rightbar.module.css'
+
+const rightbar = () => {
+  return (
+    <div className={styles.container}>
+        rightbar
+    </div>
+  )
+}
+
+export default rightbar
+
