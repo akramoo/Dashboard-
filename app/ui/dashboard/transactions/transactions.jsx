aa350fe109a313bc8dcbfc36a98d3457e3e822a8
akramoo <samirounis2@gmail.com>
2024-03-17 16:12:33 +0100

    creating transactions component

---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,12 @@
+import styles from './transactions.module.css'
+
+const transactions = () => {
+  return (
+    <div className={styles.container}>
+        transactions
+    </div>
+  )
+}
+
+export default transactions
+
