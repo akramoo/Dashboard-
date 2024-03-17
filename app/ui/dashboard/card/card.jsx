a2ee8828f5b99cd44236aa05bf05d7d60f0137ed
akramoo <samirounis2@gmail.com>
2024-03-17 18:22:30 +0100

    Creating the card compont and its style

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,12 +1,19 @@
-import styles from './card.module.css'
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 
 const Card = () => {
   return (
     <div className={styles.container}>
-        card
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.276</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12%</span>more then previous week
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
-
+export default Card;
