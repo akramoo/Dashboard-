da5d2b1f3682929947561d14145ae62e8e75db83
akramoo <samirounis2@gmail.com>
2024-03-17 18:23:06 +0100

    Adding the card, Transactions and Rightbar to the Dashboard component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,27 @@
+import Card from "../ui/dashboard/card/card.jsx";
+import Chart from "../ui/dashboard/chart/chart";
+import Transactions from "../ui/dashboard/transactions/transactions.jsx";
+import Rightbar from "../ui/dashboard/rightbar/rightbar.jsx";
+
+import styles from "../ui/dashboard/dashboard.module.css";
+
 const Dashboard = () => {
-    return (
-      <div>
-        <Card
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
       </div>
-    )
-  }
-  
-  export default Dashboard
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
