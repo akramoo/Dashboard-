321c44cd87bcefcc7aa9bcde34cd57e3c808d161
akramoo <samirounis2@gmail.com>
2024-03-19 23:37:44 +0100

    creating the users page design

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,7 @@
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const UsersPage = () => {
   return (
@@ -11,7 +12,50 @@ const UsersPage = () => {
           <button className={styles.addButton}>Add Now</button>
         </Link>
       </div>
-      <table className={styles.table}></table>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John doe
+              </div>
+            </td>
+            <td>john@gmail:com</td>
+            <td>13.01.2012</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
