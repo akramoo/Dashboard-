13156180180c39fdb45333518edc6b6ca01da8e6
akramoo <samirounis2@gmail.com>
2024-03-22 01:20:24 +0100

    Changin the view button link

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -46,7 +46,7 @@ const ProductsPage = () => {
               <td>72</td>
               <td>
                 <div className={styles.buttons}>
-                  <Link href="/">
+                  <Link href="/dashboard/products/test2">
                     <button className={`${styles.button} ${styles.view}`}>
                       View
                     </button>
