ea4dfb7fbe46f3f92b85ecc7024087fb9af65c9d
akramoo <samirounis2@gmail.com>
2024-03-22 01:19:07 +0100

    Adding Login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,12 @@
+import styles from "@/app/ui/login/login.module.css";
+import LoginForm from "../ui/login/loginForm/loginForm";
+
 const LoginPage = () => {
-    return (
-      <div>Login page</div>
-    )
-  }
-  
-  export default LoginPage
+  return (
+    <div className={styles.container}>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
