f21c37e6f4fb2eee4be5abdbcc047b38ceec0727
akramoo <samirounis2@gmail.com>
2024-04-01 00:06:37 +0100

    adding count to calculate the length of the page

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,12 +1,16 @@
 import { User } from "./modules/user.js";
 import { connectToDB } from "./utils.js";
 
-export const fatchUsers = async (q) => {
+export const fatchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
   try {
     connectToDB();
-    const users = await User.find({ username: { $regex: regex } });
-    return users;
+    const count = await User.find().count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
   } catch (error) {
     throw new Error("Failed to fatch users !");
   }
