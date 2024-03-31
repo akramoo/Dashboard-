5027dc394063e26765a2f8b787c33db312f01708
akramoo <samirounis2@gmail.com>
2024-03-31 23:41:45 +0100

    filtering and finding the user based on the search

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,12 +1,13 @@
-import { User } from "./modules/user.js"
-import { connectToDB } from "./utils.js"
+import { User } from "./modules/user.js";
+import { connectToDB } from "./utils.js";
 
-export const fatchUsers = async () => {
-    try {
-        connectToDB()
-        const users = await User.find();
-        return users;
-    } catch (error) {
-        throw new Error("Failed to fatch users !")
-    }
-}
+export const fatchUsers = async (q) => {
+  const regex = new RegExp(q, "i");
+  try {
+    connectToDB();
+    const users = await User.find({ username: { $regex: regex } });
+    return users;
+  } catch (error) {
+    throw new Error("Failed to fatch users !");
+  }
+};
