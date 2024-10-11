4daa9ba8d23115730cdcd9a3a8745a5ec5f4ba57
akramoo <samirounis2@gmail.com>
2024-10-11 11:49:23 +0100

    added a function to fetch pproduct from db

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,3 +1,4 @@
+import { Product } from "./modules/product.js";
 import { User } from "./modules/user.js";
 import { connectToDB } from "./utils.js";
 
@@ -13,5 +14,21 @@ export const fatchUsers = async (q, page) => {
     return { count, users };
   } catch (error) {
     throw new Error("Failed to fatch users !");
+  }
+};
+
+export const fatchProducts = async (q, page) => {
+  const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
+  try {
+    connectToDB();
+    const count = await Product.find().count();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    
+    return { count, products };
+  } catch (error) {
+    throw new Error("Failed to fatch Product !");
   }
 };
