1ffaca19e9a6374c30b2b5ec7a973c66ad5975b4
akramoo <samirounis2@gmail.com>
2024-10-11 15:31:55 +0100

    Refactor user link in UsersPage component to use dynamic user id

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -51,7 +51,7 @@ const UsersPage = async ({ searchParams }) => {
               <td>{user.isActive ? "active" : "passive"}</td>
               <td>
                 <div className={styles.buttons}>
-                  <Link href="/dashboard/users/test">
+                  <Link href={`/dashboard/users/${user.id}`}>
                     <button className={`${styles.button} ${styles.view}`}>
                       View
                     </button>
