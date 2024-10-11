bfb0507807098976695e01b0a9674719d33db0cb
akramoo <samirounis2@gmail.com>
2024-10-11 13:34:00 +0100

    Refactor form action in data.js to separate user and product functionality

---
+++ app/lib/data/userData.js
@@ -0,0 +1,27 @@
+import { User } from "./modules/user.js";
+import { connectToDB } from "./utils.js";
+
+export const fatchUsers = async (q, page) => {
+  const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
+  try {
+    connectToDB();
+    const count = await User.find().count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
+  } catch (error) {
+    throw new Error("Failed to fatch users !");
+  }
+};
+
+export const fatchUser = async (id) => {
+  try {
+    connectToDB();
+    const user = await User.findById(id);
+    return  user;
+  } catch (error) {
+    throw new Error("Failed to fatch user !");
+  }
+};
